bcfce2424e3ced4f7aa73eff23ee55eeb1f4ab95
Helmanz <helcze4@gmail.com>
2023-05-16 23:14:59 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+width = 300
+height = 200
+screen = pygame.display.set_mode((width, height))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
